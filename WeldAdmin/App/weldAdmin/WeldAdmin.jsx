6e2b0ec39edde2aa1ac0f376f78470908664b55e
Gunnar Siréus <gunnar.sireus@gmail.com>
2018-07-15 07:34:32 +0200

    Refactoring

--- WeldAdmin/App/weldAdmin/WeldAdmin.jsx
+++ WeldAdmin/App/weldAdmin/WeldAdmin.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Alert from '../common/Alert.jsx';
 import Input from '../common/Input.jsx';
 import { Panel, Grid, Row, Col, Glyphicon, Button, ButtonGroup } from 'react-bootstrap';
-import { fail } from 'assert';
+import 'babel-polyfill'
 
 class NameForm extends React.Component {
     constructor(props) {
@@ -45,6 +45,7 @@ class NameForm extends React.Component {
         this.doAlert('Namn ej ändrat');
         event.preventDefault();
     }
+
 
     getAjax() {
         let that = this;
@@ -91,7 +92,7 @@ class NameForm extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Name: <Input type={'text'} value={this.state.nameText} maxLength={50} id={'inputName'} value={this.state.inputName} checkError={this.handleChange} />
+                    Name: <Input type={'text'} value={this.state.inputName} maxLength={50} id={'inputName'}  checkError={this.handleChange} />
                 </label>
                 <input type="submit" value="Spara" />
             </form>
@@ -103,73 +104,8 @@ class NameForm extends React.Component {
 export default class WeldAdmin extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            nameText: props.nameText
-        };
-        this.checkInput = this.checkInput.bind(this);
-        this.getAjax = this.getAjax.bind(this);
-        this.putAjax = this.putAjax.bind(this);
-        this.doAlert = this.doAlert.bind(this);
     };
 
-    componentDidMount() {
-        this.getAjax();
-    }
-
-    componentWillReceiveProps(newProps) {
-        if (this.state.nameText !== newProps.nameText) {
-            this.setState({ nameText: newProps.nameText });
-            console.log('newProps.nameText ' + newProps.nameText);
-        };
-    };
-
-    doAlert(text) {
-        ReactDOM.render(<Alert value={text} />, document.getElementById("divAdminIndex"));
-    };
-
-    checkInput(event) {
-        if (event.target.value !== 'Donald Duck') {
-            this.setState({ nameText: event.target.value });
-            return true;
-        }
-        return false;
-    };
-
-    getAjax() {
-        let that = this;
-        $.ajax({
-            url: 'https://simple-rest-weld.herokuapp.com/user/' + this.state.nameText,
-            type: 'GET',
-            contentType: 'application/json',
-            success: function (result) {
-                that.setState({ nameText: result.name });
-            },
-            error: function (result) {
-                that.doAlert('Kan ej kontakta server ' + result.name);
-                that.setState({ nameText: 'Error' });
-            }.bind(this)
-        });
-    }
-
-    putAjax() {
-        if (this.state.nameText === 'Donald Duck') return;
-        let that = this;
-        $.ajax({
-            url: 'https://simple-rest-weld.herokuapp.com/user/',
-            type: 'PUT',
-            data: JSON.stringify(this.state.nameText),
-            contentType: 'application/json',
-            statusCode: {
-                204: function () {
-                    alert('namn ändrat till: ' + that.state.nameText);
-                }
-            },
-            error: function (result) {
-                that.doAlert('Kan ej kontakta server ' + result.name);
-                that.setState({ nameText: 'Error' });
-            }.bind(this)
-        });
-    }
 
     render() {
         return <div className="container">
@@ -193,13 +129,6 @@ export default class WeldAdmin extends Component {
                             <Col md={10}>
                                 <h1>Weld Your own App administration:</h1>
                                 <NameForm />
-                                <br />
-                                <br />
-                                <Row>
-                                    <Input checkError={this.checkInput} className="col-xs-3 col-md-3 col-sm-3 BorderInset" type={'text'} value={this.state.nameText} maxLength={50} id={'nameText'} />
-                                    <Col md={3} sm={3} xs={3} />
-                                </Row>
-                                <button onClick={() => { this.putAjax() }}>Spara</button>
                                 <div id="divAdminIndex"></div>
                             </Col>
                         </Row>
